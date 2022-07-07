a292b78681bb08c7ffa3cc904a47bdaebb4bd6f7
Marc Gutman <43051639+icanhasmath@users.noreply.github.com>
2022-07-07 15:09:43 -0500

    Add a fallback option for builtins
    Builtins may require an explicit import.

--- setup.py
+++ setup.py
@@ -36,7 +36,14 @@ class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+        try:
+            __builtins__.__NUMPY_SETUP__ = False
+        except:
+            try:
+                import builtins
+                builtins.__NUMPY_SETUP__ = False
+            except:
+                warn("Ensure numpy is installed.")
         import numpy
         self.include_dirs.append(numpy.get_include())
 
