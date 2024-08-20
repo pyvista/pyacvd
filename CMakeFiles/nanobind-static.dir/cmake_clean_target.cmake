388bc69b0a2c6e275f9fe6204c05147bcaee2c73
Alex Kaszynski <akascap@gmail.com>
2024-08-19 23:01:58 -0600

    remove unnecessary cmake files; use single source versioning

--- CMakeFiles/nanobind-static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libnanobind-static.a"
-)
--- src/pyacvd/__init__.py
+++ src/pyacvd/__init__.py
@@ -1,3 +1,11 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from pyacvd.clustering import Clustering
 
-__all__ = ["Clustering"]
+try:
+    __version__ = version("pyacvd")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+
+__all__ = ["Clustering", "__version__"]
